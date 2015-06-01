baeac0a89bf9f4d1962fd778c58ec999dc5ea749
Rajesh <rajesht1989@gmail.com>
2015-06-01 18:21:02 +0530

    translation repo and source code added

---
+++ iTransliteration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTransliteration
+//
+//  Created by Rajesh on 6/1/15.
+//  Copyright (c) 2015 Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTransliteration/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  iTransliteration
+//
+//  Created by Rajesh on 6/1/15.
+//  Copyright (c) 2015 Org. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor colorWithRed:.82 green:.9 blue:.92 alpha:1]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
